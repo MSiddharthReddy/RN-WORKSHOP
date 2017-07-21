e347dbab77e160cb9c55349d14481a31ecf16201
Siddharth Malkireddy <smalkireddy@paypal.com>
2017-07-21 08:53:53 -0700

    Implemented Axios and swiperFile

--- src/components/swiperFile.js
+++ src/components/swiperFile.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Actions } from 'react-native-router-flux';
-import { Content, Button } from './CommonComponents';
+import { Button, Content } from './CommonComponents';
 import {
   ActivityIndicator,
   View,
-  Text,
   Dimensions,
   StyleSheet,
   Animated
@@ -13,20 +13,61 @@ import {
 export default class Swiper extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      visible: false,
+      data: []
+    };
+  }
+
+  getProduct(food, place) {
+    console.log(food, place);
+    axios
+      .post('https://morning-hamlet-88026.herokuapp.com/realData', {
+        food: food,
+        location: place
+      })
+      .then(response => {
+        console.log(response);
+        this.response = response;
+        this.setState({
+          visible: !this.state.visible,
+          data: response.data.slice(0, 5)
+        });
+      });
   }
 
   componentWillMount() {
     console.log(this.props);
+    this.getProduct(this.props.food, this.props.place);
   }
 
+  renderProduct() {
+    if (!this.state.visible) {
+      return (
+        <View>
+          <ActivityIndicator
+            animating={!this.state.visible}
+            color="blue"
+            size="large"
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View >
+        <Content> Records Returned {this.state.data.length} </Content>
+        <Button
+        onPress={() => Actions.initial()}
+        > Next </Button>
+    </View>
+  );
+    }
+  }
 
   render() {
     return (
       <View style={styles.backgroundStyle}>
-        <Content> Swiper {this.props.food} {this.props.place}</Content>
-        <Button
-        onPress={() => Actions.list()}
-        > Next </Button>
+        {this.renderProduct()}
       </View>
     );
   }
