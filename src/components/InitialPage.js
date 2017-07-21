4f09e752c60563a88749c47e83d6bcfb7ac0fd28
Siddharth Malkireddy <smalkireddy@paypal.com>
2017-07-21 07:10:04 -0700

    initial page start

--- src/components/InitialPage.js
+++ src/components/InitialPage.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Content, Button } from './CommonComponents';
 import { Actions } from 'react-native-router-flux';
+import { View, TextInput, Dimensions } from 'react-native';
+import { Button, Content } from './CommonComponents';
 
-const InitialPage = () =>
-<View style={{ paddingTop: 100 }}>
-  <Content> Initial </Content>
-  <Button
-  onPress={() => Actions.card()}
-  > Next </Button>
-</View>;
+export default class InitialPage extends Component {
+  constructor() {
+    super();
+  }
 
-export default InitialPage;
+  render() {
+    const { container, textBox } = styles;
+
+    return (
+      <View style={container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#00EEEE',
+    alignItems: 'center',
+    alignContent: 'space-around',
+    flexDirection: 'column',
+    paddingTop: 150
+  },
+  textBox: {
+    height: 40,
+    backgroundColor: '#E0FFFF',
+    borderColor: '#79CDCD',
+    borderWidth: 0.3,
+    borderRadius: 10,
+    shadowColor: '#79CDCD',
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 1,
+    shadowOpacity: 0.5,
+    width: Dimensions.get('window').width - 60,
+    marginHorizontal: 30,
+    marginVertical: 15,
+    textAlign: 'center'
+  }
+};
